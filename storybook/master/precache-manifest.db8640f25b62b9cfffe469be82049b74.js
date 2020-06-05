5e3ce33786e7c2960eba4491d5cff3e84c9e63cf
beautyfree <beautyfree@users.noreply.github.com>
2020-06-05 01:55:15 +0000

    Deploy storybook for master.

---
+++ storybook/master/precache-manifest.db8640f25b62b9cfffe469be82049b74.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f69dc13c3cb7f8431fc7b441215f749",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d8327b3bbfe672ac9e08.bundle.js"
+  },
+  {
+    "url": "runtime~main.d8327b3bbfe672ac9e08.bundle.js"
+  },
+  {
+    "url": "vendors~main.d8327b3bbfe672ac9e08.bundle.js"
+  },
+  {
+    "url": "vendors~main.d8327b3bbfe672ac9e08.bundle.js.LICENSE.txt"
+  }
+]);
